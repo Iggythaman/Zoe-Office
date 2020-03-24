e55aef8a5466fe03b832bfa8e918a021ae1ac24b
Osei Bonsu <osei.bonsu@gmail.com>
2020-03-24 18:49:47 -0500

    fix stuff

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Index() {
+  return (
+    <Layout>
+      <div>
+        <p>Home page</p>
+      </div>
+    </Layout>
+  );
+}
